44285fee446db250903b15a6d95f7bc97077ecaa
张伟宝 <jzwb@hotmail.com>
2024-05-18 18:30:57 +0800

    类加载子系统

---
+++ src/main/java/com/jzwbxx/jvm/ClassLoaderTest.java
@@ -0,0 +1,27 @@
+package com.jzwbxx.jvm;
+
+/**
+ * 类加载子系统
+ */
+public class ClassLoaderTest {
+    public static void main(String[] args) {
+        ClassLoaderTest t = new ClassLoaderTest();
+        //对象由哪个类创建
+        System.out.println(t.getClass());
+        //获取是谁加载这个类，sun.misc.Launcher$AppClassLoader@18b4aac2
+        System.out.println(t.getClass().getClassLoader());
+
+        ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
+        System.out.println("sysClassLoader：" + sysClassLoader);//sun.misc.Launcher$AppClassLoader@18b4aac2
+
+        ClassLoader extClassLoader = sysClassLoader.getParent();
+        System.out.println("extClassLoader：" + extClassLoader);//sun.misc.Launcher$ExtClassLoader@5cad8086
+
+        ClassLoader bootstrapClassLoader = extClassLoader.getParent();
+        System.out.println("bootstrapClassLoader：" + bootstrapClassLoader);//试图获取引导类加载器，结果为null
+
+        //String类是使用引导类加载器进行加载的->java的核心类库都是使用引导类加载器加载
+        ClassLoader stringClassLoader = String.class.getClassLoader();
+        System.out.println("stringClassLoader：" + stringClassLoader);//null
+    }
+}
