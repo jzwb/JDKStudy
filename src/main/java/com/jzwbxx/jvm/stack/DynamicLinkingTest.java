5534e1dffb8fde79628f0e20d984272a628d8a83
张伟宝 <jzwb@hotmail.com>
2024-05-20 08:38:27 +0800

    动态链接

--- src/main/java/com/jzwbxx/jvm/stack/DynamicLinkingTest.java
+++ src/main/java/com/jzwbxx/jvm/stack/DynamicLinkingTest.java
@@ -2,6 +2,15 @@ package com.jzwbxx.jvm.stack;
 
 /**
  * 栈帧-动态链接
+ * 动态链接的主要作用是支持方法调用过程中的符号引用（Symbolic Reference）解析
  */
 public class DynamicLinkingTest {
+    public static void main(String[] args) {
+        DynamicLinkingTest dynamicLinkingTest = new DynamicLinkingTest();
+        dynamicLinkingTest.printMsg();
+    }
+    
+    public void printMsg(){
+        System.out.println("DynamicLinkingTest printMsg method!");
+    }
 }
